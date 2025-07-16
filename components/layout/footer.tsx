b003400dfb9e1f865e4aeee517171d5216b1687d
alexandreshey <4lexandreshey@gmail.com>
2025-07-16 07:27:13 -0300

    mensagem descritiva sobre a alteração

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -4,7 +4,8 @@ export function Footer() {
   return (
     <footer className="border-t border-border pt-3 pb-4 bg-card flex justify-center items-center">
       <p className="text-center text-sm text-muted-foreground">
-        &copy; {currentYear} Cultivo Indoor Analytics. Feito com 🌱 e código.
+        &copy; {currentYear} Cultivo Indoor Analytics. Feito com 🌱 e código.<br />
+        <span className="text-xs">feito por heyalexandre</span>
       </p>
     </footer>
   )
